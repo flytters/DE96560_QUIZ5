d4a200ef24a740e681e5360c6bb6f15498bcb544
flytters <74450634+flytters@users.noreply.github.com>
2020-11-15 22:01:09 +0800

    uploaded Quiz5 (loop) folder

---
+++ Quiz5-1/Quiz5-1.asm
@@ -0,0 +1,22 @@
+				#include<p18F4550.inc>
+
+
+				org	0x00
+				goto start
+				org	0x08
+				retfie
+				org	0x18
+				retfie
+
+start			MOVLW	B'00000001'
+				MOVWF	0x07,A	
+				MOVWF	0x06,A
+				MOVWF	0x05,A
+				MOVWF	0x04,A
+				MOVWF	0x03,A
+				MOVWF	0x02,A
+				MOVWF	0x01,A
+				MOVWF	0x00,A
+			
+				
+				end
